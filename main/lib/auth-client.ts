49fdda73db1e7f97617c3f8db59ed9590c13eb02
Aldanov Daniyal <danial.aldanov@gmail.com>
2026-07-13 09:37:46 +0500

    baseurl changed

--- main/lib/auth-client.ts
+++ main/lib/auth-client.ts
@@ -2,7 +2,13 @@ import { createAuthClient } from "better-auth/react";
 import { inferAdditionalFields } from "better-auth/client/plugins";
 import type { auth } from "./auth";
 
+const baseUrl = process.env.BETTER_AUTH_URL;
+
+if (!baseUrl) {
+  throw new Error("base url undefined");
+}
+
 export const authClient = createAuthClient({
-  baseURL: "http://localhost:3000",
+  baseURL: baseUrl,
   plugins: [inferAdditionalFields<typeof auth>()],
 });
