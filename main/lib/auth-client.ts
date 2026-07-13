b975749f172e1448f9488caa5e629d04de9cb0e3
Aldanov Daniyal <danial.aldanov@gmail.com>
2026-07-13 09:57:55 +0500

    env var change

--- main/lib/auth-client.ts
+++ main/lib/auth-client.ts
@@ -2,7 +2,7 @@ import { createAuthClient } from "better-auth/react";
 import { inferAdditionalFields } from "better-auth/client/plugins";
 import type { auth } from "./auth";
 
-const baseUrl = process.env.BETTER_AUTH_URL;
+const baseUrl = process.env.NEXT_PUBLIC_BETTER_AUTH_URL;
 
 if (!baseUrl) {
   throw new Error("base url undefined");
